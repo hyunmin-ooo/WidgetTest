771be6ea965e05bdaa5ea9c0c5f28589a2ffa43e
hyunmin-ooo <2201110357@office.kopo.ac.kr>
2025-03-24 16:26:11 +0900

    4장 기본위젯의 속성들

--- app/src/main/java/kr/ac/kopo/widgettest/MainActivity.java
+++ app/src/main/java/kr/ac/kopo/widgettest/MainActivity.java
@@ -1,6 +1,12 @@
 package kr.ac.kopo.widgettest;
 
+import static kr.ac.kopo.widgettest.R.*;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +15,32 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    RadioGroup rg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(layout.enable_clickable_test);
+
+//        rg = findViewById(R.id.rg);
+//
+//
+//        Button btnDone = findViewById(R.id.btn_done);
+//
+//        btnDone.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String result = "";
+//                if(rg.getCheckedRadioButtonId() == id.rb_male){
+//                    result = getResources().getString(R.string.male);
+//                }else
+//                    result = getResources().getString(R.string.female);
+//
+//
+//                Toast toast = Toast.makeText(getApplicationContext(), result + "를 선택하였습니다.", Toast.LENGTH_LONG);
+//                toast.show();
+//            }
+//        });
     }
 }
